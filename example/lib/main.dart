bc5dbf380725bf09865b0f399125106c0806c620
owennwang <814209329@qq.com>
2023-02-16 10:06:02 +0800

    feat: reademe

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:tencent_open_file/tencent_open_file.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,7 +15,8 @@ class _MyAppState extends State<MyApp> {
   var _openResult = 'Unknown';
 
   Future<void> openFile() async {
-    final filePath = '/storage/emulated/0/update.apk';
+    final filePath = '/Users/wangrunlin/Library/Developer/CoreSimulator/Devices/D5CE0385-9B66-4203-B5F7-1EDECB03C278/data/Containers/Data/Application/254EB815-49E5-43EB-9BDA-4E9217B67FDF/Documents/Simulator\ Screen\ Shot\ -\ iPhone\ 14\ Pro\ -\ 2022-11-14\ at\ 16.36.45.png';
+    getApplicationDocumentsDirectory().then((value) => print(value));
     final result = await OpenFile.open(filePath);
 
     setState(() {
